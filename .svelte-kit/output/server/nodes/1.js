d6f51d7bd7eff4f0021e917678ee2df4a96b11e6
IEdCmWty <drop.rest.inside@gmail.com>
2026-01-10 03:58:05 +0200

    Deploy static site - субота, 10 січня 2026 03:58:05 +0200

--- .svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.Cn54XFwB.js","_app/immutable/chunks/B-lBlEvR.js","_app/immutable/chunks/DVHnvNiM.js","_app/immutable/chunks/Dfp_T4QS.js","_app/immutable/chunks/DETSXESc.js","_app/immutable/chunks/Brf-UXat.js","_app/immutable/chunks/kDOZtEjq.js","_app/immutable/chunks/BuZs-ncu.js","_app/immutable/chunks/CjcB4Qx5.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/output/server/nodes/3.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 3;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/demo/paraglide/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/3.DnlzL1qr.js","_app/immutable/chunks/B-lBlEvR.js","_app/immutable/chunks/DVHnvNiM.js","_app/immutable/chunks/Dfp_T4QS.js","_app/immutable/chunks/DETSXESc.js","_app/immutable/chunks/Brf-UXat.js","_app/immutable/chunks/DKi_IBpn.js"];
-export const stylesheets = [];
-export const fonts = [];
